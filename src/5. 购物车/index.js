dceb969f1f32bc51cfc2f37c040a25e039382c34
whalefall541 <jackchen541@sina.com>
2022-01-07 00:12:38 +0800

    2022-01-07 00:12:38

--- src/5. 购物车/index.js
+++ src/5. 购物车/index.js
@@ -1,12 +1,47 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		
+		list: [
+			{
+				id: 1,
+				name:'iPhone 12',
+				price: 6799,
+				count: 1
+			},
+			{
+				id: 2,
+				name:'iPad Pro',
+				price: 5888,
+				count: 1
+			},
+			{
+				id: 3,
+				name:'MacBook Pro',
+				price: 24999,
+				count: 1
+			}
+		]
 	},
 	computed: {
-		
+		totalPrice: function() {
+			var total = 0;
+			for (var i = 0; i < this.list.length; i ++) {
+				var item = this.list[i];
+				total += item.price * item.count;
+			}
+			return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
+		}
 	},
 	methods: {
-		
+		handleReduce: function(index) {
+			if (this.list[index].count === 1) return;// 防止非按钮行的修改引发问题
+			this.list[index].count--;
+		},
+		handleAdd: function(index) {
+			this.list[index].count++;
+		},
+		handleRemove: function(index) {
+			this.list.splice(index, 1);
+		},
 	}
 });
