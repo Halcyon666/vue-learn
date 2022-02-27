39637501ac88abb944daaed2b30351b3c5fb4d3e
whalefall541 <jackchen541@sina.com>
2022-02-27 16:04:43 +0800

    route is ok

---
+++ src/7.组件/School.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="yell">
+        <h2>学校名称：{{ schoolName }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+        <button @click="showName">点我提示学校名称</button>
+    </div>
+</template>
+
+<script>
+// 暴露组件的配置对象 省略Vue.extend()
+     export default {
+        name: 'School',
+        data() {
+            return {
+                schoolName:'清华大学',
+                address:'北京市海淀区双清路30号'
+            }
+        },
+        methods: {
+            showName() {
+                alert(this.schoolName)
+            }
+        },
+    }
+   
+</script>
+
+<style>
+    .yell {
+        background-color: aqua;
+    }
+</style>
---
+++ src/7.组件/Student.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>学生姓名：{{ stuName }}</h2>
+        <h2>学生班级：{{ stuClass }}</h2>
+        <button @click="showStuClass">点我提示学生班级</button>
+    </div>
+</template>
+
+<script>
+// 暴露组件的配置对象 省略Vue.extend()
+     export default {
+        name: 'Student',
+        data() {
+            return {
+                stuName:'张三',
+                stuClass:'高三一班'
+            }
+        },
+        methods: {
+            showStuClass() {
+                alert(this.stuClass)
+            }
+        },
+    }
+   
+</script>
